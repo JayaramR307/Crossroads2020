142ca0a79354f6daa5e3134828eef360e4b53c58
JayaramR307 <61595757+JayaramR307@users.noreply.github.com>
2020-03-07 20:12:50 +0530

    Add files via upload

---
+++ cr/eventreg.php
@@ -0,0 +1,23 @@
+   <!DOCTYPE html>
+   <html>
+   <head>
+     <title>Events</title>
+     <link rel="stylesheet" type="text/css" href="css/parall.css">
+   </head>
+    <body>
+            <div>
+              
+            </div>
+            <div class="parallax-wrapper">
+                <div class="content">
+                      <p>Event Picture</p>
+                </div>
+            </div>
+            <div class="regular-wrapper">
+                <div class="content">
+                     <p>Even Description.</p>
+                </div>
+            </div>
+    </body>
+   </html>
+
